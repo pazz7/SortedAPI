296b0a1001e4dbddfb99186d8497ab1b26f7e722
Pacita Bantilan <pbantilan@gmail.com>
2024-03-18 20:14:52 +0800

    Add project files.

---
+++ Controllers/RainfallController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sorted.Application;
+using Sorted.Domain.Rainfall;
+
+namespace SortedAPI.Controllers
+{
+    [Route("api/rainfall")]
+    [ApiController]
+    public class RainfallController(
+        GetRainfallReadingsByStationIdUseCase getRainfallReadingsByStationIdUseCase,
+        ILogger<RainfallController> logger) : ControllerBase
+    {
+        [HttpGet("id/{stationId}/readings")]
+        public async Task<ActionResult<RainfallReadingResponse>> GetRainfall(string stationId, int count = 10)
+        {
+            RainfallReadingResponse response = await getRainfallReadingsByStationIdUseCase.ExecuteAsync(stationId, count);
+            return Ok(response);
+        }
+    }
+}
